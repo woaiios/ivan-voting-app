b8feff9112cb212eae393d0121e63869030dfe0e
Glitch (ivan-voting-app) <none>
2018-05-20 04:54:32 +0000

    👩‍❤️‍💋‍👩🏤 Updated with Glitch

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Item from './components/item'
+
+ReactDOM.render(
+  (<div> 
+    <Item/>
+  </div>), 
+  document.getElementById('root'))
---
+++ app/components/item.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const Item = function() {
+  return (
+   <div> <h1>Hi!ookkk</h1>  </div> 
+  )
+};
+
+module.exports = Item;
